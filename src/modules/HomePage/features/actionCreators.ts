e1f9aa3b9daeb28f589cb950305c13c0f3a3f679
KyryloP <peresada1997@gmail.com>
2023-03-30 13:24:25 +0300

    rename action

--- src/modules/HomePage/features/actionCreators.ts
+++ src/modules/HomePage/features/actionCreators.ts
@@ -3,7 +3,7 @@ import { IApiError, HomePageAPI } from "../../../api";
 import { HOME_PAGE_SLICE_NAME, IGetBannerPhotoResponse, IGetGalleryPhotosDTO, IGetGalleryPhotosResponse } from "./models";
 
 export const getGalleryPhotos = createAsyncThunk<IGetGalleryPhotosResponse[], IGetGalleryPhotosDTO, {serializedErrorType: IApiError}>(
-  `${HOME_PAGE_SLICE_NAME}/getPhotos`,
+  `${HOME_PAGE_SLICE_NAME}/getGalleryPhotos`,
   async function(data: IGetGalleryPhotosDTO){
     const response = await HomePageAPI.getGalleryPhotos(data);
     return response.data;
