3bfa726c86ff06fe722d469c4920900123314e97
KyryloP <peresada1997@gmail.com>
2023-04-07 17:48:44 +0300

    user card

--- src/modules/UserCard/features/actionCreators.ts
+++ src/modules/UserCard/features/actionCreators.ts
@@ -3,7 +3,7 @@ import { IApiError, UserCardAPI } from "../../../api";
 import { IGetUserInfoDTO, IGetUserInfoResponse, USER_CARD_SLICE_NAME } from "./models";
 
 export const getUserInfo = createAsyncThunk<IGetUserInfoResponse, IGetUserInfoDTO, {serializedErrorType: IApiError}>(
-  `${USER_CARD_SLICE_NAME}/getPhoto`,
+  `${USER_CARD_SLICE_NAME}/getUserInfo`,
   async function(data: IGetUserInfoDTO) {
     const response = await UserCardAPI.getUserInfo(data);
     return response.data;
